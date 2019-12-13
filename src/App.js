417ddc67f0ae64b69fa418c49ed7afecbef7ef89
Ryan McDonagh <ryan@henschen.com>
2019-12-13 17:06:01 -0500

    added card component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+//import axios from 'axios';
 
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-            monsters: [
-                {
-                    name: 'Frankenstein',
-                    id: 'asdasd'
-                },
-                {
-                    name: 'Dracula',
-                    id: 'sadsad'
-                },
-                {
-                    name: 'Zombie',
-                    id: 'wqeqe'
-                }
-            ]
+            monsters: []
         };
     }
+
+
+    // With Fetch API
+     componentDidMount() {
+         fetch('https://jsonplaceholder.typicode.com/users')
+         .then(response => response.json())
+         .then(users => this.setState({ monsters: users}))
+     }
+
+    // With axios
+    // componentDidMount() {
+    //     axios.get('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => this.setState({ monsters: response.data}))
+    // }
 
 
     render() {
         return ( 
             <div className = "App" >
-                {
-                    this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-                }
+                <CardList monsters={this.state.monsters} />
             </div>
         );
     }
