1867751c4904e035588603cb709df27ec651d238
Ryan McDonagh <ryan@henschen.com>
2019-12-16 11:26:46 -0500

    Updates

---
+++ precache-manifest.225b01f0f05eeeb085825db1a92c4a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c37574d4a529c288b6b2b8fd0f7be0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "eb04301e7c71ed7c42c6",
+    "url": "/monster-rolodex/static/css/main.3e9fd796.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb04301e7c71ed7c42c6",
+    "url": "/monster-rolodex/static/js/main.b45a1137.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
